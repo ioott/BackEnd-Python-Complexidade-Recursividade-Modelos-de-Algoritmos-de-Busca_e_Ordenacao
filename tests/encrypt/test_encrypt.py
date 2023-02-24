a8b87b5fe415ed2875ffead0a0079cc77e3012f9
ioott <vioott@gmail.com>
2023-02-24 14:37:04 -0300

    test: visa garantir que a função de criptografia encrypt_message respeite uma lógica específica

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,21 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    test_message = "testmessage"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(test_message, "key")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123456, 1)
+
+    not_in_range = encrypt_message(test_message, 15)
+    assert not_in_range == "egassemtset"
+
+    key_is_odd = encrypt_message(test_message, 5)
+    assert key_is_odd == "mtset_egasse"
+
+    key_is_pair = encrypt_message(test_message, 4)
+    assert key_is_pair == "egassem_tset"
