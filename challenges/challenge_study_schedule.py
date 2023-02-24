3498ee07883fe93b48ff0fa3399bc20bc362cf55
ioott <vioott@gmail.com>
2023-02-24 10:43:51 -0300

    feat: cria a função study_schedule, que determina qual o melhor horário para disponibilizar os conteúdos de estudo, baseada na qtd de estudantes presentes na escola Utilize a estratégia de resolução de problemas chamada força bruta, em que a função será chamada várias vezes, com valores diferentes para a variável target_time, e serão analisados os retornos da função.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    n = len(permanence_period)
+    counter = 0
+    for index in range(0, n):
+        start, end = permanence_period[index]
+        if (type(start) is not int or type(end) is not int
+                or type(target_time) is not int):
+            return None
+        elif start == '' or end == '':
+            return None
+        elif start <= target_time <= end:
+            counter += 1
+    return counter
